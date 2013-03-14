ba2c86c716d495f530837f0afd936040846a3371
thomaseger <mail@thomaseger.de>
2013-03-14 16:24:32 +0100

    Fixed indentiation

--- core/model.go
+++ core/model.go
@@ -3,7 +3,7 @@ package core
 import "fmt"
 
 type Model struct {
-	Theses []Thesis
+  Theses []Thesis
 }
 
 type Thesis struct {
@@ -18,22 +18,22 @@ type Argument struct {
 }
 
 func NewModel() *Model {
-	return mockModel()
+  return mockModel()
 }
 
 func mockModel() *Model {
-	var model Model
-	for i := 0; i < 10; i++ {
-		thesis := Thesis {
-			Text: fmt.Sprintf("Thesis %d", i),
-		}
-		for j := 0; j < 10; j++ {
-			argument := Argument {
-				Text: fmt.Sprintf("Argument %d", j),
-			}
-			thesis.Arguments = append(thesis.Arguments, argument)
-		}
-		model.Theses = append(model.Theses, thesis);
-	}
-	return &model
+  var model Model
+  for i := 0; i < 10; i++ {
+    thesis := Thesis {
+      Text: fmt.Sprintf("Thesis %d", i),
+    }
+    for j := 0; j < 10; j++ {
+      argument := Argument {
+        Text: fmt.Sprintf("Argument %d", j),
+      }
+      thesis.Arguments = append(thesis.Arguments, argument)
+    }
+    model.Theses = append(model.Theses, thesis);
+  }
+  return &model
 }
