b917c26a6d9b1959a9788118d28b71988b5f75f8
thomaseger <mail@thomaseger.de>
2013-03-18 11:18:50 +0100

    Removed delegate. Encapsulated server functionality in arguments.go

--- arguments.go
+++ arguments.go
@@ -1,26 +1,45 @@
 package main
 
 import (
-	"arguments/delegate"
+	"arguments/service"
 	"arguments/core"
+	"code.google.com/p/gorest"
+	"net/http"
 	"flag"
 	"fmt"
 	"log"
 )
 
-func parseCmd() (string, int) {
+type Arguments struct {
+	address string
+	model *core.Model
+}
+
+func (a *Arguments) parseCmd() {
 	host := flag.String("host", "", "Specifies the host.")
 	port := flag.Int("port", 4000, "Specifies the port to listen on.")
 	flag.Parse()
-	return *host, *port
+
+	a.address = fmt.Sprintf("%s:%d", *host, *port)
+}
+
+func (a *Arguments) serve() {
+	a.model = core.NewModel()
+
+	thesesService := service.NewThesesService(a.model)
+	gorest.RegisterService(thesesService)	    
+    
+    http.Handle("/", gorest.Handle())
+
+    log.Print("Running arguments! on ", a.address)   
+    error := http.ListenAndServe(a.address, nil)
+    if(error != nil) {
+    	log.Fatal("Error while serving: ", error)
+    }
 }
 
 func main() {
-	host, port := parseCmd()
-	address := fmt.Sprintf("%s:%d", host, port)
-
-	log.Print("Running arguments! on ", address)
-
-	model := core.NewModel();
-	delegate.Serve(address, model)
+	args := new(Arguments)
+	args.parseCmd()
+	args.serve()
 }
