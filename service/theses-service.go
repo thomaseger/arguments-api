0316bd67e95aff540a0051160a6eb2f622806d4a
thomaseger <mail@thomaseger.de>
2013-03-18 14:21:11 +0100

    Removed unnecessary comment.

--- service/theses-service.go
+++ service/theses-service.go
@@ -11,7 +11,6 @@ type ThesesService struct {
 
 	gorest.RestService `root:"/api" consumes:"application/json" produces:"application/json"`
 
-	//GET Endpoints
 	theses gorest.EndPoint `method:"GET" path:"/theses" output:"[]Thesis"`
 	thesis gorest.EndPoint `method:"GET" path:"/theses/{thesisId:int}" output:"Thesis"`
 	arguments gorest.EndPoint `method:"GET" path:"/theses/{thesisId:int}/arguments" output:"[]Argument"`
